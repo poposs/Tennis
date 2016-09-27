06e5e01e67f8cb80f5be682342653382fb8ef4a4
Panya Wangduangthawan <57660009@go.buu.ac.th>
2016-09-28 00:12:03 +0700

    ADD File Jasmine AND Tennis_Test

---
+++ spec/tennis.spec.js
@@ -0,0 +1,15 @@
+function Tennis(A,B) {
+    
+  this.start_game = function(){
+    return "Love-Love";
+  }
+
+};
+
+describe("Tennis()", function() {
+
+  it('should be "Love-Love" when Start game', function() {
+    var echoScore = new Tennis();
+    expect(echoScore.start_game()).toBe('Love-Love');
+  });
+});
